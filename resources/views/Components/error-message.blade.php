c2751abf40bd60f1557830a9d2250c44da98af16
Okan Aslan <aaslan.okan@gmail.com>
2024-04-17 16:43:49 +0300

    add error-success messages view

---
+++ resources/views/Components/error-message.blade.php
@@ -0,0 +1,5 @@
+@if (session()->has('error'))
+    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <span class="font-medium">{{session('error')}}
+    </div>
+@endif
---
+++ resources/views/Components/success-message.blade.php
@@ -0,0 +1,6 @@
+@if (session()->has('success'))
+    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+        role="alert">
+        <span class="font-medium">{{session('success')}}
+    </div>
+@endif
